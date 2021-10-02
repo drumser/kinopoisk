a40916fdb495276b215652e79f7f14c7d26699c2
quantick <mail@kladov.xyz>
2021-10-02 12:07:43 +0300

    remove default kinopoisk url from configuration

--- src/main/kotlin/ru/quantick/kinopoisk/movie/configuration/movieprovider/KinopoiskProviderConfiguration.kt
+++ src/main/kotlin/ru/quantick/kinopoisk/movie/configuration/movieprovider/KinopoiskProviderConfiguration.kt
@@ -6,5 +6,5 @@ import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties(prefix = "movie-provider.kinopoisk")
 data class KinopoiskProviderConfiguration @ConstructorBinding constructor(
-    val baseUrl: String = "https://api.ott.kinopoisk.ru/v12"
+    val baseUrl: String
 )
